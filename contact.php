d8dc2a38ffb7d35ccbd108e5a5b1bf3c1b933e40
UD-sht <udshrestha48@gmail.com>
2023-10-02 19:09:59 +0545

    commit

---
+++ contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Us</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Contact Us</u></h3>
+            
+            <div class="contact">
+            <h3 style="color:#2E4372;"><u>Kathmandu Branch</u></h3>
+            <p><span class="heading">Address - </span>KMC School, Bagbazar, Kathmandu.</p>
+            <p><span class="heading">Tel - </span>01-111111/12</p>
+            <p><span class="heading">Email - </span>kathmandubranch@onlinebank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Lalitpur Branch</u></h3>
+            <p><span class="heading">Address - </span>DAV School, bhanimandal, Jawalakhel, Lalitpur.</p>
+            <p><span class="heading">Tel - </span>01-2222222/32</p>
+            <p><span class="heading">Email - </span>lalitpurbranch@onlinebank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Bhaktapur Branch</u></h3>
+            <p><span class="heading">Address - </span>NIST School, Near shawtachowki, lokanthali, Bhaktapur.</p>
+            <p><span class="heading">Tel - </span>01-3333333/11</p>
+            <p><span class="heading">Email - </span>bhaktapurbranch@onlinebank.com</p>
+            </div>
+            </div>
---
+++ t&c.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Safe Online Banking</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Terms and conditions</u></h3>
+              
+            <div class="contact" style="overflow: scroll;">
+            1. The Laws and Regulation of Nepal and customs and procedures common to Banks in the country will apply to and govern the conduct of accounts opened with the Bank.<br>
+2. The Bank reserves the right to amend these rules at any time and in any manner which the Bank deems necessary with or without notice to the applicants or the public.<br>
+3. Commissions and/or service charge will be levied by the Bank as applicable.<br>
+4. The funds in an account would be considered by the Bank to be security for all the obligations present or future of the account holder to the Bank and in the event of the dishonor of such obligations the Bank is entitled to utilize such funds against the obligations of the account holder to the Bank without notice to the account holder.<br>
+5. The account holder must maintain the prescribed minimum balance as set by the Bank from time to time.<br>
+6. Statement of account will be provided on demand.<br>
+7. If there is no transaction in the account for a period of 36 months or more, the account becomes dormant/inactive. The account holder has to be self-present to make the account operative.<br>
+8. The Bank will have the right of set off.<br>
+The Bank shall endorse a cheque as ‘Good for Payment’ as per the request of its customer where certain charge will be levied as per the Bank’s rule.<br>
+9. Cheque book for account will be provided to the accounts maintaining the certain minimum balance as stipulated by the Bank. Cheque book to a third party will be delivered only after obtaining the identity document of the receiver and authority from the account holder.<br>
+10. The Bank shall charge applicable fee in case of cheque return due to insufficient balance in the customer’s account.<br>
+11. The customers are advised to contact the Bank for detail information regarding account operation and other service charges like minimum balance requirement for account opening, charges for duplicate statement of account, account balance certificate, account closing charge, returned cheque and good for payment charge and other details. The bank will debit the customer’s account for the applicable charges on its service.<br>
+12. The Bank acts only as collecting agent and assumes no responsibility for the realization of any item deposited with the bank for collection. Proceeds of cheques or their instruments deposited with the Bank are not available for withdrawal until collected by the Bank. The Bank reserves the right to debit any of accounts that may have been exceptionally credited with an item subsequently unpaid on collection. The Bank may refuse to accept for collection cheques drawn in favor of a third party or if the payee’s name is not identically to Bank’s record.<br>
+13. The customers are advised to count their cash withdrawn at Bank’s counter in front of the tellers. The Bank shall not be held responsible for any shortfall or losses in cash withdrawn that are not counted at the Bank’s counter in front of the tellers.<br>
+14. Bank shall have the right to refuse to open an Account or close/block existing Account at any time if the Bank feels the transaction of an account as doubtful/suspicious without providing notice to this effect.<br>
+15. Postdated and stale cheque will not be paid.<br>
+The Bank reserves the right to close/freeze the deposit accounts with or without notice if the conduct of the account is not satisfactory in the opinion of the Bank or any other reasons whatsoever.<br>
+16. The Bank will take utmost care to record all the entries correctly in the account of the constituent(s), but in case of any error the Bank reserve right to make the correct, adjusting entries without notice.<br>
+17. Any change in address and/or constitution of the constituent(s) should be immediately communicated to the Bank through a reliable media and acknowledgment shall be received from the Bank.<br>
+18. With the prior approval of the bank the account holder can withdraw the amount through withdrawal slip supplied by the Bank, which should be signed as per specimen signature supplied to the Bank. However, Bank do not encourage to withdraw through withdrawal slip.<br>
+19.The Bank will register the constituent(s)’ stop payment request of cheques, but cannot guarantee its nonpayment.<br>
+20. Interest on overdrawn account will be charged on daily product basis at the prevailing rates.<br>
+21. The account holder should not draw any cheque in excess of its available balance. The Bank may blacklist the account holder at the request of payee of cheque issued by him in case of non-availability of balance to honor such cheque as per Nepal Rastra Bank Guidelines.<br>
+22. Bank has right to close accounts having zero balance more than six months.<br>
+23. No interest shall be paid on the matured time deposit. However, Bank may renew matured time deposit upon its expiry at the request of the account holder.<br>
+24. The applicable interest rate on deposit account is subjected to change from to time. The changed rate shall be informed publishing on newspaper and/or displaying on Banks website.<br>
+            </div>
+        </div>
+<?php include 'footer.php'; ?>
